f206dfd111d9f0cbf05e534c10a4d0e5d5ca65c5
Michail Chourdas <34147163+Polilogas@users.noreply.github.com>
2023-01-29 03:17:18 +0200

    Add classes on click for the nav animation

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function openNav() {
   var mobileLinkList = document.getElementById("mobile-link-list");
+  var headerMiddleLine = document.querySelector(".middle-line");
+  var headerTopLine = document.querySelector(".top-line");
+  var headerBottomLine = document.querySelector(".bottom-line");
   
   if (mobileLinkList.style.display === "block") {
     mobileLinkList.style.display = "none";
+    headerMiddleLine.classList.remove("middle-line-animation-start");
+    headerTopLine.classList.remove("top-line-animation-start");
+    headerBottomLine.classList.remove("bottom-line-animation-start");
+    
   } else {
     mobileLinkList.style.display = "block";
-    mobileLinkList.style.paddingTop = "35px";
-    mobileLinkList.style.textAlign = "right";
+    headerMiddleLine.classList.add("middle-line-animation-start");
+    headerTopLine.classList.add("top-line-animation-start");
+    headerBottomLine.classList.add("bottom-line-animation-start");
   }
 }
