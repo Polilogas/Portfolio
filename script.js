63af3aa938c5c2395d8a2d4b7418e3a675e845e8
Michail Chourdas <34147163+Polilogas@users.noreply.github.com>
2023-01-29 05:38:35 +0200

    Add and animate full screen mobile menu

--- script.js
+++ script.js
@@ -3,17 +3,39 @@ function openNav() {
   var headerMiddleLine = document.querySelector(".middle-line");
   var headerTopLine = document.querySelector(".top-line");
   var headerBottomLine = document.querySelector(".bottom-line");
+  var navBackground = document.getElementById("mobile-menu-background");
+  var logo = document.querySelector(".logoPicture");
+  var mobileMenuList = document.getElementById("mobile-menu-list");
+  
+  var homeLink = document.querySelector(".home");
+  var aboutLink = document.querySelector(".about");
+  var projectLink = document.querySelector(".projects");
+  var skillsLink = document.querySelector(".skills");
   
   if (mobileLinkList.style.display === "block") {
+    // Remove class when closing
     mobileLinkList.style.display = "none";
     headerMiddleLine.classList.remove("middle-line-animation-start");
     headerTopLine.classList.remove("top-line-animation-start");
     headerBottomLine.classList.remove("bottom-line-animation-start");
+    navBackground.style.display = "none";
+    logo.classList.remove("logo-animation-start");
+    mobileMenuList.style.display = "none";
+    homeLink.classList.remove("home-link-animation-start");
     
   } else {
+    // Add class when opening
     mobileLinkList.style.display = "block";
     headerMiddleLine.classList.add("middle-line-animation-start");
     headerTopLine.classList.add("top-line-animation-start");
     headerBottomLine.classList.add("bottom-line-animation-start");
+    navBackground.style.display = "flex";
+    navBackground.classList.add("background-animation-start");
+    logo.classList.add("logo-animation-start");
+    mobileMenuList.style.display = "flex";
+    homeLink.classList.add("home-link-animation-start");
+    aboutLink.classList.add("about-link-animation-start");
+    projectLink.classList.add("projects-link-animation-start");
+    skillsLink.classList.add("skills-link-animation-start");
   }
 }
