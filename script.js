e22912789914f02f890a58618eab48671a7de607
Michail Chourdas <34147163+Polilogas@users.noreply.github.com>
2023-02-03 06:58:02 +0200

    Add function for the tabs in projects section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This is for the mobile navigation bar
 function openNav() {
   var mobileLinkList = document.getElementById("mobile-link-list");
   var headerMiddleLine = document.querySelector(".middle-line");
@@ -55,3 +56,21 @@ function openNav() {
     skillsLink.classList.add("skills-link-animation-start");
   }
 }
+
+// This is for the tabs in projects section
+function openProject(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on to open the tab on page load
+document.getElementById("defaultOpen").click();
